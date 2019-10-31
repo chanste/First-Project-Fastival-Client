131c4711ae19476e55da37e9da28380c8c3c2a87
Kim Hyung Chan <phano@Kims-MacBook-Pro.local>
2019-10-31 22:35:19 +0900

    (컴포넌트 추가) 로그인 구현 완료, 주석 리팩토링 남음

---
+++ client/components/LoadingScreen.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import {View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+import firebase from 'firebase'
+
+ class LoadingScreen extends Component {
+     componentDidMount() {
+         this.checkIfLoggedIn();
+     }
+
+    checkIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged(function(user){
+            if(user){
+                this.props.navigation.navigate('DashboardScreen')
+            }
+            else{
+                this.props.navigation.navigate('LoginScreen')
+            }
+        }.bind(this))
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" />
+            </View>
+        )
+    }
+}
+/*
+사용자가 로그인 상태가 아니라면 currentUser 값이 null입니다.
+var user = firebase.auth().currentUser;
+
+if (user) {
+  // User is signed in.
+} else {
+  // No user is signed in.
+}
+*/
+
+export default LoadingScreen;
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+
+

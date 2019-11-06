dd3ceb536d57908716ada976e1fd5319102b90c5
HyunGu Lee <lhg1628@gmail.com>
2019-11-06 18:47:03 +0900

    commit before pull upstream

--- client/App.js
+++ client/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { createAppContainer ,createSwitchNavigator } from 'react-navigation'
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import LoginScreen from './Eachcomponents/LoginScreen'
-import LoadingScreen from './Eachcomponents/LoadingScreen'
-// import Index from './Eachcomponents/Index'
-import Main from './Screens/Main'
+import LoginScreen from "./Eachcomponents/LoginScreen";
+import LoadingScreen from "./Eachcomponents/LoadingScreen";
+import Index from "./Eachcomponents/Index";
+import Main from "./Screens/Main";
 
-import firebase from 'firebase'
-import {firebaseConfig} from './config'
-firebase.initializeApp(firebaseConfig)
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+firebase.initializeApp(firebaseConfig);
 
 export default class App extends Component {
   render() {
@@ -21,25 +21,25 @@ const AppSwitchNavigator = createSwitchNavigator({
   LoadingScreen: LoadingScreen,
   LoginScreen: LoginScreen,
   Main: Main // 메인
-})
+});
 
 const AppNavigator = createAppContainer(AppSwitchNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
